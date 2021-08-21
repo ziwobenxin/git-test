a9e83cd30a8f8eec765f9f9637135f722a7a621d
layne <Layne@atguigu.com>
2021-08-21 16:32:01 +0800

    fist commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello gitl!");
+        System.out.println("hello gitl!");
     }
 }
